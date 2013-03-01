ac5147d2befd7c4aba00f927e5f96265ef54df27
vagrant <vagrant@precise32.(none)>
2013-03-01 12:48:04 +0000

    Removed the accidental commit

--- spike-paypal/README.md
+++
@@ -1,4 +0,0 @@
-spike-paypal
-============
-
-A quick spike to work out how to interact with PayPal.
--- spike-paypal/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SpikePaypal::Application.config.secret_token = 'b8a6610912678887e96893cb1643fd77d1508c8aea462f97a8a6eac98a261800365c04b392c7e2077c4e3396a44bf0986912515835b60f25f7225f130f23d134'
