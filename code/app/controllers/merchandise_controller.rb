dc807d2c64d5a60c126972330ed8407e243695e6
Colin Orr <colin.orr@gmail.com>
2013-03-21 16:37:25 +0000

    Added the shopping basket link to the Merchandise page

--- code/app/controllers/merchandise_controller.rb
+++ code/app/controllers/merchandise_controller.rb
@@ -3,10 +3,18 @@ class MerchandiseController < ApplicationController
 
   layout 'administration'
 
+  def count_basket_items
+    if Basket.exists?(session[:basket_id])
+      return Basket.find(session[:basket_id]).items.count
+    end
+    return nil
+  end
+
   # GET /products
   # GET /products.json
   def index
     @products = Product.order(:priority).reverse_order
+    @basket_size = count_basket_items
 
     respond_to do |format|
       format.html # index.html.erb
