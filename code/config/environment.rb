f8907f2de6844337442b745b7f8d667386791c2a
ColinOrr <colin.orr@gmail.com>
2013-03-22 14:37:27 +0000

    Added the cookie key to the secrets

--- code/config/environment.rb
+++ code/config/environment.rb
@@ -3,7 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Load my secret settings
 SECRET = YAML.load_file("#{Rails.root}/config/secrets.yml")
-raise 'Please update /config/secrets.yml with you own settings' if SECRET['self_destruct']
+raise 'Please update /config/secrets.yml with your own settings' if SECRET['self_destruct']
 
 # Initialize the rails application
 Myapp::Application.initialize!
