b72ba910e5a45a3420a580e2ccddbac899357619
ColinOrr <colin.orr@gmail.com>
2013-03-23 18:20:17 +0000

    Added a label to the product

--- code/app/models/product.rb
+++ code/app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :category, :description, :image, :name, :price, :priority, :quantity
+  attr_accessible :category, :description, :image, :name, :price, :priority, :quantity, :label
   validates :name, :price, :category, :presence => true
   validates :price, :numericality => { :greater_than => 0 }
 end
--- code/db/schema.rb
+++ code/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130319203937) do
+ActiveRecord::Schema.define(:version => 20130323181024) do
 
   create_table "baskets", :force => true do |t|
     t.string   "shopper"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20130319203937) do
     t.integer  "priority",    :default => 0
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
+    t.string   "label"
   end
 
 end
